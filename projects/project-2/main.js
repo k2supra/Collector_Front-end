9255e4dddde32db9655028829504b45b66e3b6eb
Deviatyi Andrii <the_na17@student.itstep.org>
2025-02-08 15:11:51 +0200

    added 'add' and 'remove' functions for cart

--- projects/project-2/main.js
+++ projects/project-2/main.js
@@ -1,5 +1,3 @@
-hideModalWindow();
-
 const products = [
     { name: "Hoodie Oversize", price: 1200, img: "hoodie.jpg" },
     { name: "T-Shirt base", price: 600, img: "tshirt.jpg" },
@@ -86,30 +84,73 @@ function showModalWindow() {
     getModalWindow().style.visibility="visible";
 }
 
-function getCart() {
-    return document.getElementById("cart-content");
+
+function getCartList() {
+    return document.querySelector("#cart-content .list ul");
 }
 
-addToCart();
-function addToCart() {
-    getCard();
-}
 
-function getCard() {
+/* get card */
+( () => {
     let buttons = document.querySelectorAll(".add-to-cart");
     for (const item of buttons) {
         item.addEventListener('click', function (event) {
-            // console.log(event.target.parentNode.parentNode.parentNode)
             const getName = event.target.parentNode.parentNode.children[0].textContent;
             const getPrice = truncateString(event.target.parentNode.parentNode.children[1].textContent);
             let getImage = `${event.target.parentNode.parentNode.parentNode.children[0].getAttribute("src")}`.split("/")[1];
-            
-            console.log("PROCE::::::::::::", getName, getPrice, getImage);
-            console.log("FIND::::", products.find((item) => item.name == getName && item.price == getPrice && item.img == getImage));
-            // return products.find((item) => item.name == getName && item.price == getPrice && item.img == getImage);
+            // console.log("FIND:::::", products.find((item) => item.name == getName && item.price == getPrice && item.img == getImage));
+
+            createCartCard(products.find((item) => item.name == getName && item.price == getPrice && item.img == getImage));
         })
     }
+})();
+
+/* delete cart item */
+function deleteItem(item)
+{
+    let productItem = item.parentNode;
+    productItem.remove();
+};
+
+function createCartCard(item)
+{
+    let li = document.createElement("li");
+
+    const cardItem = document.createElement("div");
+    cardItem.classList.add("item");
+
+    li.appendChild(cardItem);
+
+    const img = document.createElement("img");
+    img.setAttribute("src", `images/${item.img}`);
+
+    cardItem.appendChild(img);
+
+    const description = document.createElement("div");
+    description.classList.add("description");
+    
+    const h4 = document.createElement("h4");
+    h4.textContent = item.name;
+
+    description.appendChild(h4);
+
+    const price = document.createElement("span");
+    price.textContent = item.price;
+
+    description.appendChild(price);
+
+    cardItem.appendChild(description);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "-"
+    deleteButton.setAttribute("onclick", "deleteItem(this)");
+
+    li.appendChild(deleteButton);
+
+    getCartList().appendChild(li);
 }
+
 function truncateString(string) {
     return string.substring(0, string.length - 1);
 }
