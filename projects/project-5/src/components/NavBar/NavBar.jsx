648f4464dfd48f6ddcbfb4c1dffd2339fcdd7b72
k2supra <andrijkorolevic@gmail.com>
2025-09-01 20:09:44 +0300

    feature(navBar): added navBar for mobile version

---
+++ projects/project-5/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import './navBar.css'
+
+function NavBar() {
+    return (
+    <nav>
+        <div className="logo"></div>
+        <div className="burger"></div>
+    </nav>
+    )
+}
+
+export default NavBar
